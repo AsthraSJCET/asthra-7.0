67f36f3033c33a88983ae0872b1a757da7b92c3f
Vishnu Nair P <--help>
2022-11-10 22:38:10 +0530

    Test commit

--- src/index.js
+++ src/index.js
@@ -11,8 +11,6 @@ const root = createRoot(document.getElementById('root'));
 const domain = process.env.REACT_APP_AUTH0_DOMAIN;
 const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
 
-
-
 root.render(
     // <React.StrictMode>
         <Auth0Provider
