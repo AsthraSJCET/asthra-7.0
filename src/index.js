5d9dc656a2353a2e206b5bfe29d9b0b757142b63
Vishnu Nair P <vishnunairp@outlook.com>
2022-11-10 22:39:19 +0530

    test

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import './stylesheets/index.css';
 import { Auth0Provider } from '@auth0/auth0-react';
 import {Helmet} from "react-helmet";
 
+
 const root = createRoot(document.getElementById('root'));
 const domain = process.env.REACT_APP_AUTH0_DOMAIN;
 const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
