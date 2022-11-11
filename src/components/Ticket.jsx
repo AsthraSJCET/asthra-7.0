7b8e82b4d0146385fd1ebf3b7a65f8837e133417
reenphygeorge <reenphygeorge@gmail.com>
2022-11-11 18:59:10 +0530

    feat: ticket added

---
+++ src/components/Ticket.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+
+function Ticket({ EventName, Description, Date, Time }) {
+  return (
+    <div className="mt-10 lg:mt-0">
+      <div className="bg-lime-500 rounded-t-lg w-96 lg:w-auto lg:h-96 mr-40 pb-10 lg:pb-0">
+        <div className="px-10 pt-10">
+          <h4 className="font-bold font-spaceGrotesk text-sm lg:text-md">
+            WORKSHOP
+          </h4>
+          <h1 className="font-bold font-spaceGrotesk text-md lg:text-4xl">
+            {EventName}
+          </h1>
+        </div>
+        <div className="grid grid-cols-2">
+          <div className="px-10 pt-10">
+            <h4 className="font-spaceGrotesk text-sm lg:text-md">VENUE</h4>
+            <h1 className="font-bold font-spaceGrotesk text-md lg:text-2xl">
+              SEMINAR HALL, SJC
+            </h1>
+          </div>
+          <div className="px-10 pt-10">
+            <h4 className="font-spaceGrotesk text-sm lg:text-md">
+              DATE AND TIME
+            </h4>
+            <h1 className="font-bold font-spaceGrotesk text-md mb-0 lg:text-2xl">
+              {Date} <br />
+              {Time}
+            </h1>
+            <br />
+            <h1 className="font-bold font-spaceGrotesk mt-0 text-md lg:text-2xl"></h1>
+          </div>
+        </div>
+        <div className="px-10 pt-10">
+          <h4 className="font-spaceGrotesk text-sm lg:text-md">NAME</h4>
+          <h1 className="font-bold font-spaceGrotesk text-md lg:text-2xl">
+            MOHAMMED YASIM
+          </h1>
+        </div>
+      </div>
+      <div className="bg-zinc-800 rounded-b-lg h-40 mr-40">
+        <div className="px-10 pt-10 grid grid-cols-2">
+          <div className="grid justify-items-start">
+            <img src="/images/asthra-logo.svg" className="lg:h-20 h-12" />
+          </div>
+          <div className="grid justify-items-end">
+            <img src="/images/asthra.svg" className="lg:h-20 h-14" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Ticket;
--- src/pages/eventPage.jsx
+++ src/pages/eventPage.jsx
@@ -4,14 +4,13 @@ import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 import React, { useEffect, useState } from "react";
 import { publicAPI } from "../etc/api";
-
-
+import Ticket from "../components/Ticket";
 
 const Capture = () => {
-  const [data, setData] = useState([])
-  const [loading, setLoading] = useState(true)
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
   const search = useLocation().search;
-  const code = new URLSearchParams(search).get('c');
+  const code = new URLSearchParams(search).get("c");
 
   useEffect(() => {
     publicAPI
@@ -25,9 +24,10 @@ const Capture = () => {
   }, [code]);
 
   const rules_formatted = String(data.rules);
-  const rules = typeof rules_formatted === "string" ? rules_formatted.split(';') : ""
+  const rules =
+    typeof rules_formatted === "string" ? rules_formatted.split(";") : "";
 
-
+  console.log(data);
   return (
     <>
       <Navbar />
@@ -35,52 +35,91 @@ const Capture = () => {
         <div>
           <div className="max-w-screen lg:px-20 md:p-8">
             <h1 className="text-5xl font-spaceGrotesk text-white font-bold pb-4 tracking-tight">
-              {data.name} {(data.active ? "" : ` (Registration closed)`)}
+              {data.name} {data.active ? "" : ` (Registration closed)`}
             </h1>
             <hr className="bg-white" />
-            <p className="text-white font-spaceGrotesk">        Seats left:&nbsp;
-            <span className=" text-[#CCFF00] text-xl font-bold">{data.event_seat}</span>
-              </p>
-
-          </div>
-          <div className="lg:px-20 py-4 font-spaceGrotesk text-white">
-            <h3 className="font-bold font-mono text-2xl pb-6">DESCRIPTION</h3>
-            <p className="px-4 text-white font-spaceGrotesk text-sm tracking-normal font-semibold max-w-xl">
-              {data.desc}
+            <p className="text-white font-spaceGrotesk">
+              {" "}
+              Seats left:&nbsp;
+              <span className=" text-[#CCFF00] text-xl font-bold">
+                {data.event_seat}
+              </span>
             </p>
           </div>
-          <div className="lg:px-20 py-4 font-spaceGrotesk text-white">
-            <h3 className="font-bold font-mono text-2xl pb-6">RULES</h3>
-            {/* <p className="text-white font-spaceGrotesk text-sm tracking-normal font-semibold max-w-xl">
-              {data.rules}
-            </p> */}
-            <div className="px-4 text-white font-spaceGrotesk text-sm tracking-normal font-semibold max-w-xl">
-              {rules.map((rules, key) => (
-                <p key={key}>{key + 1}. {rules} </p>
-              ))}
+          <div className="grid lg:grid-cols-2 grid-rows-2">
+            <div>
+              <div className="lg:px-20 py-4 font-spaceGrotesk text-white">
+                <h3 className="font-bold font-mono text-2xl pb-6">
+                  DESCRIPTION
+                </h3>
+                <p className="px-4 text-white font-spaceGrotesk text-sm tracking-normal font-semibold max-w-xl">
+                  {data.desc}
+                </p>
+              </div>
+              <div className="lg:px-20 py-4 font-spaceGrotesk text-white">
+                <h3 className="font-bold font-mono text-2xl pb-6">RULES</h3>
+                {/* <p className="text-white font-spaceGrotesk text-sm tracking-normal font-semibold max-w-xl">
+                {data.rules}
+              </p> */}
+                <div className="px-4 text-white font-spaceGrotesk text-sm tracking-normal font-semibold max-w-xl">
+                  {rules.map((rules, key) => (
+                    <p key={key}>
+                      {key + 1}. {rules}{" "}
+                    </p>
+                  ))}
+                </div>
+              </div>
+              <div className="lg:px-20 py-4 font-spaceGrotesk text-white">
+                <h3 className="font-bold font-mono text-2xl pb-6">CONTACT</h3>
+                {/* <p className="px-4 text-white font-spaceGrotesk text-sm tracking-normal font-semibold max-w-xl">
+              </p> */}
+                <div className="px-4">
+                  <p className=" text-md font-spaceGrotesk">
+                    {data.cordinator1_name}
+                  </p>
+                  <div className=" px-2 flex flex-row">
+                    <p className="text-sm font-spaceGrotesk">
+                      {data.cordinator1_contact} &nbsp;
+                    </p>
+                    <p className="text-sm font-spaceGrotesk">
+                      {data.cordinator1_email}
+                    </p>
+                  </div>
+
+                  <p className=" text-md font-spaceGrotesk">
+                    {data.cordinator2_name}
+                  </p>
+                  <div className=" px-2 flex flex-row">
+                    <p className="text-sm font-spaceGrotesk">
+                      {data.cordinator2_contact} &nbsp;
+                    </p>
+                    <p className="text-sm font-spaceGrotesk">
+                      {data.cordinator2_email}
+                    </p>
+                  </div>
+
+                  <p className=" text-md font-spaceGrotesk">
+                    {data.cordinator3_name}
+                  </p>
+                  <div className=" px-2 flex flex-row">
+                    <p className="text-sm font-spaceGrotesk">
+                      {data.cordinator3_contact} &nbsp;
+                    </p>
+                    <p className="text-sm font-spaceGrotesk">
+                      {data.cordinator3_email}
+                    </p>
+                  </div>
+                </div>
+              </div>
             </div>
-          </div>
-          <div className="lg:px-20 py-4 font-spaceGrotesk text-white">
-            <h3 className="font-bold font-mono text-2xl pb-6">CONTACT</h3>
-            {/* <p className="px-4 text-white font-spaceGrotesk text-sm tracking-normal font-semibold max-w-xl">
-            </p> */}
-            <div className="px-4">
-
-              <p className=" text-md font-spaceGrotesk">{data.cordinator1_name}</p>
-              <div className=" px-2 flex flex-row"><p className="text-sm font-spaceGrotesk">{data.cordinator1_contact} &nbsp;</p>
-                <p className="text-sm font-spaceGrotesk">{data.cordinator1_email}</p></div>
-
-              <p className=" text-md font-spaceGrotesk">{data.cordinator2_name}</p>
-              <div className=" px-2 flex flex-row"><p className="text-sm font-spaceGrotesk">{data.cordinator2_contact} &nbsp;</p>
-                <p className="text-sm font-spaceGrotesk">{data.cordinator2_email}</p></div>
-
-              <p className=" text-md font-spaceGrotesk">{data.cordinator3_name}</p>
-              <div className=" px-2 flex flex-row"><p className="text-sm font-spaceGrotesk">{data.cordinator3_contact} &nbsp;</p>
-                <p className="text-sm font-spaceGrotesk">{data.cordinator3_email}</p></div>
-
+            <div>
+              <Ticket
+                EventName={data.name}
+                Description={data.desc}
+                Date={data.date}
+                Time={data.time}
+              />
             </div>
-
-
           </div>
         </div>
       </div>
