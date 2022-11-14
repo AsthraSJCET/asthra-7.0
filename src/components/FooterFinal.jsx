727631eb66585f35543f6da7f1a2cef2876977ed
Rajat Mathew <rajatxmathew@gmail.com>
2022-11-14 18:35:19 +0530

    Added images, banner, footer

---
+++ src/components/FooterFinal.jsx
@@ -0,0 +1,66 @@
+// import "./styles.css";
+ 
+export default function App() {
+  return (
+    <div classNameName="App">
+ 
+    <footer className="bg-zinc-900 font-spaceGrotesk">
+      <div className="container mx-auto flex flex-col justify-end gap-10 p-10 lg:flex-row">
+        <div className="first max-w-xs">
+          <img className="max-w-full" src="https://res.cloudinary.com/djzshuwo1/image/upload/v1668420358/Frame_83_d7g5la.png" alt="" />
+          <p className="text-white">Asthra - The national level technical fest of St. Joseph's College of Engineering and Technology, Palai</p>
+        </div>
+ 
+        <div className="third">
+          <ul className="text-white/70">
+            <h4 className="text-xl font-bold text-white">LINKS</h4>
+            <li><a className="text-xl hover:text-gray-300" href="#">ABOUT</a></li>
+            <li><a className="text-xl hover:text-gray-300" href="#">WORKSHOPS</a></li>
+            <li><a className="text-xl hover:text-gray-300" href="#">EXHIBITIONS</a></li>
+            <li><a className="text-xl hover:text-gray-300" href="#">EVENTS</a></li>
+            <li><a className="text-xl hover:text-gray-300" href="#">SPONSORSHIPS</a></li>
+            <li><a className="text-xl hover:text-gray-300" href="#">TEAM</a></li>
+            <li><a className="text-xl hover:text-gray-300" href="#">CONTACT</a></li>
+          </ul>
+        </div>
+ 
+        <div className="second">
+          <h4 className="text-xl font-bold text-white">VISION</h4>
+          <p className="text-lg text-gray-300">Developing into a world-className, pace-setting institute of Engineering and Technology with distinct identity and character, meeting the goals and aspirations of the society.</p>
+          <h4 className="mt-5 text-xl font-bold text-white">MISSION</h4>
+          <ul className="text-lg text-gray-300">
+            <li>To maintain a conducive infrastrucutre and learning environment for world-className education.</li>
+            <li>To nurture a team of dedicated, competent and research-oriented faculty.</li>
+            <li>To develop students with moral and ethical values, for their successful career by offering variety of programmes and services</li>
+          </ul>
+        </div>
+      </div>
+      <div className="flex justify-center gap-3 py-5">
+        <a hhref="https://instagram.com/asthra_sjcet">
+          <i className="fa-brands fa-instagram text-xl text-white transition-all duration-300 hover:text-teal-500"></i>
+        </a>
+ 
+        <a hhref="https://instagram.com/asthra_sjcet">
+          <i className="fa-brands fa-facebook text-xl text-white transition-all duration-300 hover:text-teal-500"></i>
+        </a>
+ 
+        <a hhref="https://instagram.com/asthra_sjcet">
+          <i className="fa-brands fa-youtube text-xl text-white transition-all duration-300 hover:text-teal-500"></i>
+        </a>
+ 
+        <a hhref="https://instagram.com/asthra_sjcet">
+          <i className="fa-brands fa-linkedin text-xl text-white transition-all duration-300 hover:text-teal-500"></i>
+        </a>
+      </div>
+ 
+      <div className="bg-teal-900">
+        <p className="py-3 text-center text-white"> Made with 💗  by <a hhref="#" className="font-bold text-blue-300 hover:text-black">Team Asthra</a></p>
+      </div>
+ 
+    </footer>
+ 
+ 
+    </div>
+  );
+}
+
