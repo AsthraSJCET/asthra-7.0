660ce7b52aee0d680f228d41a83d599f5267e021
reenphygeorge <reenphygeorge@gmail.com>
2022-11-25 22:31:59 +0530

    feat: ticket demo

--- src/pages/tickets.jsx
+++ src/pages/tickets.jsx
@@ -4,9 +4,10 @@
 // // import Ticket from "../components/Ticket"
 // import Loader from "../lib/Loader";
 
-import React from "react"
+import React from "react";
 import { publicAPI } from "../etc/api";
-import ScrollToTop from "../lib/ScrollToTop"
+import ScrollToTop from "../lib/ScrollToTop";
+import "../stylesheets/tickets.css";
 
 // function MyTickets() {
 //     let [data, setData] = useState([]);
@@ -14,7 +15,6 @@ import ScrollToTop from "../lib/ScrollToTop"
 //     let [error, setError] = useState(false);
 //     // const context_ = useContext(AsthraContext);
 //     console.log(error, loading)
-
 
 //     useEffect(() => {
 //         setLoading(true)
@@ -46,54 +46,101 @@ import ScrollToTop from "../lib/ScrollToTop"
 //     )
 // }
 
-
 class AllTickets extends React.Component {
-    constructor(props) {
-        super(props);
-        this.load_data = this.load_data.bind(this);
-        this.state = {
-            tickets: []
-        }
-    }
-    componentDidMount(){
-        this.load_data();
-    }
-    load_data = () => {
-        publicAPI.get('/my-ticket', {
-            headers: {
-                // 'Authorization': this.props.data.user.email,
-                'Authorization': 'mohammedyasim.edkm@gmail.com',
-                'Cache-Control': 'no-cache',
-                'Pragma': 'no-cache',
-                'Expires': '0',
-            }
-        }).then(response => {
-            this.setState({ tickets: response.data })
-        }).catch(e => {
-            console.log(e);
-        })
-    }
-    render() {
-        return (<>
-            {this.state.tickets.map((ticket) => { 
-                return (<>
-                {JSON.stringify(ticket)}
-                {/* //PASS THIS Data */}
-                </>)
-                 })}
-        </>)
-    }
+  constructor(props) {
+    super(props);
+    this.load_data = this.load_data.bind(this);
+    this.state = {
+      tickets: [],
+    };
+  }
+  componentDidMount() {
+    this.load_data();
+  }
+  load_data = () => {
+    publicAPI
+      .get("/my-ticket", {
+        headers: {
+          // 'Authorization': this.props.data.user.email,
+          Authorization: "mohammedyasim.edkm@gmail.com",
+          "Cache-Control": "no-cache",
+          Pragma: "no-cache",
+          Expires: "0",
+        },
+      })
+      .then((response) => {
+        this.setState({ tickets: response.data });
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+  render() {
+    return (
+      <div className="grid lg:grid-cols-2 grid-cols-1">
+        {this.state.tickets.map((ticket) => {
+          console.log(ticket);
+          return (
+            <div>
+              {JSON.stringify(ticket)}
+              {
+                <>
+                  <div className="bg-[#CCFF00] rounded-t-lg lg:w-96 w-80 pt-20"></div>
+                  <div className="hex-wrapper lg:w-96 w-80 z-0 grid justify-center">
+                    <div className="hex z-10"></div>
+                  </div>
+                  <div className="lg:w-96 w-80 p-4 bg-zinc-800">
+                    <div className="grid grid-cols-2">
+                      <h2 className="text-white font-normal font-spaceGrotesk text-3xl lg:text-4xl">
+                        {ticket.name}
+                      </h2>
+                    </div>
+                    <div className="grid grid-cols-2">
+                      <h4 className="pb-36 pt-5 text-zinc-500 font-normal font-spaceGrotesk text-xl">
+                        {ticket.college}
+                      </h4>
+                    </div>
+                    <h1 className="text-zinc-500 font-bold font-spaceGrotesk text-4xl">
+                      {ticket.event_type}
+                    </h1>
+                    <h6 className="pb-5 text-white font-light">
+                      *something to be noted. I forgot.
+                    </h6>
+                    <div className="grid grid-rows-1 grid-cols-2 pb-5">
+                      <div className="grid justify-items-start">
+                        <img
+                          src="images/asthra_logo_white.svg"
+                          className="h-10"
+                        />
+                      </div>
+                      <div className="grid justify-items-end">
+                        <img src="images/asthra.svg" className="h-10" />
+                      </div>
+                    </div>
+                  </div>
+                </>
+              }
+              <button className="mt-3 text-center transition-all duration-300 hover:-translate-y-2 rounded font-spaceGrotesk text-1xl w-80 inline-block py-4 bg-white font-medium lg:w-96">
+                Download
+              </button>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
 }
-
 
 function MyTickets() {
-    return (<>
-        <ScrollToTop />
-        <div className="container m-auto p-5">
-            <h2 className="text-2-lg text-white">Your Tickets</h2>
-            <AllTickets />
-        </div>
-    </>)
+  return (
+    <>
+      <ScrollToTop />
+      <div className="container m-auto p-5">
+        {/* <h2 className="text-2-lg text-white">Your Tickets</h2> */}
+        <AllTickets />
+      </div>
+    </>
+  );
 }
 
-export default MyTickets
+export default MyTickets;
