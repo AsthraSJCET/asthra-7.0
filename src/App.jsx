f3dcbd95271c124c2ecd9ecfb6daad5fc6ab745c
reenphygeorge <reenphygeorge@gmail.com>
2022-11-27 04:17:42 +0530

    fix: ticket

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import { Routes, Route, BrowserRouter, Navigate } from "react-router-dom";
 import NotFound from "./pages/NotFound";
 import Capture from "./pages/eventPage";
 import { useAuth0 } from "@auth0/auth0-react";
-import { Helmet } from 'react-helmet'
-import RegisterForm from "./components/RegisterForm"
+import { Helmet } from "react-helmet";
+import RegisterForm from "./components/RegisterForm";
 import { AsthraContext } from "./etc/context";
 import Home from "./pages/Home";
 import About from "./pages/about";
@@ -23,69 +23,117 @@ class AsthraAppMain extends React.Component {
       isAuthenticated: this.props.data.isAuthenticated,
       user: this.props.data.user,
       asthra_user: null,
-      tickets: null
-    }
+      tickets: null,
+    };
     this.syncData = this.syncData.bind(this);
   }
   syncData = () => {
-    publicAPI.post('/sync-asthra', {}, {
-      headers: {
-        'Authorization': this.props.data.user.email,
-        'Cache-Control': 'no-cache',
-        'Pragma': 'no-cache',
-        'Expires': '0',
-      }
-    }).then(response => {
-      this.setState({ asthra_user: response.data.user, tickets: response.data.ticket })
-    }).catch(e => {
-      console.log(e);
-    })
-  }
+    publicAPI
+      .post(
+        "/sync-asthra",
+        {},
+        {
+          headers: {
+            Authorization: this.props.data.user.email,
+            "Cache-Control": "no-cache",
+            Pragma: "no-cache",
+            Expires: "0",
+          },
+        }
+      )
+      .then((response) => {
+        this.setState({
+          asthra_user: response.data.user,
+          tickets: response.data.ticket,
+        });
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
   componentDidMount() {
-    if (
-      this.props.data.isAuthenticated
-    ) {
+    if (this.props.data.isAuthenticated) {
       this.syncData();
     }
   }
   render() {
-    return (<main className="column">
-      <Helmet>
-        <title>Asthra 7.0</title>
-        {/* <link rel="favicon" href="https://picsum.photos/256/256" type="image/svg" /> */}
-      </Helmet>
-      <AsthraContext.Provider value={this.state}>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<IndexTemplate />}>
-              <Route index element={<Home />} />
-              <Route path="/about" element={<About />} />
-              {/* <Route path="/ambassador" element={<></>} /> */}
-              <Route path="/contact" element={<Contact></Contact>} />
+    return (
+      <main className="column">
+        <Helmet>
+          <title>Asthra 7.0</title>
+          {/* <link rel="favicon" href="https://picsum.photos/256/256" type="image/svg" /> */}
+        </Helmet>
+        <AsthraContext.Provider value={this.state}>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<IndexTemplate />}>
+                <Route index element={<Home />} />
+                <Route path="/about" element={<About />} />
+                {/* <Route path="/ambassador" element={<></>} /> */}
+                <Route path="/contact" element={<Contact></Contact>} />
 
-              <Route path="/workshops" element={<><ListPageTemplate page="workshop" /></>} />
-              <Route path="/competitions" element={<><ListPageTemplate page="competition" /></>} />
-              <Route path="/events" element={<><ListPageTemplate page="event" /></>} />
-              <Route path="/expo" element={<><ListPageTemplate page="exhibition" /></>} />
+                <Route
+                  path="/workshops"
+                  element={
+                    <>
+                      <ListPageTemplate page="workshop" />
+                    </>
+                  }
+                />
+                <Route
+                  path="/competitions"
+                  element={
+                    <>
+                      <ListPageTemplate page="competition" />
+                    </>
+                  }
+                />
+                <Route
+                  path="/events"
+                  element={
+                    <>
+                      <ListPageTemplate page="event" />
+                    </>
+                  }
+                />
+                <Route
+                  path="/expo"
+                  element={
+                    <>
+                      <ListPageTemplate page="exhibition" />
+                    </>
+                  }
+                />
 
-              <Route path="/workshop/:code" element={<Capture />} />
-              <Route path="/competition/:code" element={<Capture />} />
-              <Route path="/event/:code" element={<Capture />} />
-              <Route path="/exhibition/:code" element={<Capture />} />
+                <Route path="/workshop/:code" element={<Capture />} />
+                <Route path="/competition/:code" element={<Capture />} />
+                <Route path="/event/:code" element={<Capture />} />
+                <Route path="/exhibition/:code" element={<Capture />} />
 
-              <Route path="/tickets" element={this.props.data.isAuthenticated?<MyTickets />:<Navigate to="/" />} />
-              <Route path="/profile" element={<Profile />} />
-              <Route path="/register/:code" element={<RegisterForm />} />
+                <Route
+                  path="/tickets"
+                  element={
+                    this.props.data.isAuthenticated ? (
+                      <MyTickets />
+                    ) : (
+                      <Navigate to="/" />
+                    )
+                  }
+                />
+                {/* <Route path="/tickets" element={<MyTickets />} /> */}
 
+                <Route path="/profile" element={<Profile />} />
+                <Route path="/register/:code" element={<RegisterForm />} />
+
+                <Route path="*" element={<NotFound />} />
+              </Route>
+              <Route path="/team" element={<Team />} />
               <Route path="*" element={<NotFound />} />
-            </Route>
-            <Route path="/team" element={<Team />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </BrowserRouter>
-      </AsthraContext.Provider>
-    </main>
-    )
+            </Routes>
+          </BrowserRouter>
+        </AsthraContext.Provider>
+      </main>
+    );
   }
 }
 
@@ -96,17 +144,17 @@ function App() {
       isAuthenticated: isAuthenticated,
       user: user,
       asthra_user: null,
-      tickets: null
-    }
-    return (<>{!error ? <AsthraAppMain data={data} /> : <div>{error}</div>}</>);
+      tickets: null,
+    };
+    return <>{!error ? <AsthraAppMain data={data} /> : <div>{error}</div>}</>;
   } else {
     return (
-      <div className="text-white">Loading...
+      <div className="text-white">
+        Loading...
         <Loader />
       </div>
-    )
+    );
   }
-
 }
 
 export default App;
